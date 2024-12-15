1627b6f1b1975afff5adb204d85998dea5f351a0
NaziaSiddique <nazia.siddique04@gmail.com>
2024-12-15 11:42:30 +0000

    Practiced debugger and creating index.js.map & edit config file

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,7 @@
 "use strict";
-let age;
+let age = 20;
+if (age < 50)
+    age += 10;
+console.log(age);
+console.log(age += 10);
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-let age: number;
+let age: number = 20;
+if (age < 50)
+    age += 10
+console.log(age);
+
+// Use Debugger:
+// Create age as a variable in teh watch pallet
+// F5 for debugging
+// F10 for stepping through each line
